ffa1bb2a13db8126b96fa452778d5dd41e2d0f02
Robbert Wijtman <robbert@xinxmedia.com>
2022-05-30 13:41:17 -0700

    split out generic map stuff

--- src/components/GameMap.js
+++ src/components/GameMap.js
@@ -1,71 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Map, Layer, LayerImage } from '@bucky24/react-canvas-map';
-import { Canvas } from '@bucky24/react-canvas';
+import React, { useContext } from 'react';
 
+import TheMap from './TheMap';
 import GameContext from '../contexts/GameContext';
-import ModuleContext from '../contexts/ModuleContext';
-import ImageContext from '../contexts/ImageContext';
 
 export default function GameMap() {
-    const [size, setSize] = useState({ width: 0, height: 0 });
     const { map } = useContext(GameContext);
-    const { tiles } = useContext(ModuleContext);
-    const { images } = useContext(ImageContext);
-
-    const resize = () => {
-        setSize({
-            width: window.innerWidth,
-            height: innerHeight,
-        });
-    }
-
-    useEffect(() => {
-        window.addEventListener("resize", resize);
-        resize();
-
-        return () => {
-            window.removeEventListener("resize", resize);
-        }
-    }, []);
 
     return (
-		<Canvas width={size.width} height={size.height}>
-            <Map
-                width={size.width}
-                height={size.height}
-                x={0}
-                y={0}
-                mapBackground={{
-                    color: "#000",
-                }}
-                offMapBackground={{
-                    color: "#000",
-                }}
-            >
-                <Layer>
-                    {map.map(({ x, y, tile }) => {
-                        const tileData = tiles[tile];
-                        if (!tileData) {
-                            return null;
-                        }
-
-                        const image = images[tileData.image];
-                        if (!image) {
-                            return null;
-                        }
-
-                        return (
-                            <LayerImage
-                                x={x}
-                                y={y}
-                                src={image}
-                                width={1}
-                                height={1}
-                            />
-                        );
-                    })}
-                </Layer>
-            </Map>
-        </Canvas>
+        <TheMap
+            map={map}
+        />
     );
 }
---
+++ src/components/TheMap.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Map, Layer, LayerImage } from '@bucky24/react-canvas-map';
+import { Canvas } from '@bucky24/react-canvas';
+
+import ModuleContext from '../contexts/ModuleContext';
+import ImageContext from '../contexts/ImageContext';
+
+export default function TheMap({ map }) {
+    const [size, setSize] = useState({ width: 0, height: 0 });
+    const { tiles } = useContext(ModuleContext);
+    const { images } = useContext(ImageContext);
+
+    const resize = () => {
+        setSize({
+            width: window.innerWidth,
+            height: innerHeight,
+        });
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", resize);
+        resize();
+
+        return () => {
+            window.removeEventListener("resize", resize);
+        }
+    }, []);
+
+    return (
+		<Canvas width={size.width} height={size.height}>
+            <Map
+                width={size.width}
+                height={size.height}
+                x={0}
+                y={0}
+                mapBackground={{
+                    color: "#000",
+                }}
+                offMapBackground={{
+                    color: "#000",
+                }}
+            >
+                <Layer>
+                    {map.map(({ x, y, tile }) => {
+                        const tileData = tiles[tile];
+                        if (!tileData) {
+                            return null;
+                        }
+
+                        const image = images[tileData.image];
+                        if (!image) {
+                            return null;
+                        }
+
+                        return (
+                            <LayerImage
+                                key={`tile_${x}_${y}`}
+                                x={x}
+                                y={y}
+                                src={image}
+                                width={1}
+                                height={1}
+                            />
+                        );
+                    })}
+                </Layer>
+            </Map>
+        </Canvas>
+    );
+}
