815c8b9955b376b694360ea605cf8b158a7bce6e
Robbert Wijtman <robbert@xinxmedia.com>
2024-04-01 14:54:31 -0700

    fix portal collision

--- data/modules/main/scripts/portal_collide.js
+++ data/modules/main/scripts/portal_collide.js
@@ -7,11 +7,17 @@ let allFilled = true;
 for (const portal of portals) {
     const objects = this.game.getEntitiesAt(portal.getPos().x, portal.getPos().y);
     const characters = objects.filter((object) => object.entityType === "character");
+    // we know a character has stepped onto our current portal, so if this is our portal
+    // that triggered the event, then we know it's a hit
+    if (portal.id === this.entity.id) {
+        characters.push(this.other);
+    }
+
     if (characters.length === 0) {
         allFilled = false;
     }
 }
 
-if (!allFilled) {
+if (allFilled) {
     this.game.victory();
 }
