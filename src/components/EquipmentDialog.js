2aa739ad87e10ad99ead86d0dcbe4af23358ed7f
Robbert Wijtman <rwijtman@slack-corp.com>
2025-02-04 11:27:47 -0800

    Show unassigned equipment in the equipment dialog

--- src/components/EquipmentDialog.js
+++ src/components/EquipmentDialog.js
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import DialogBase from './DialogBase';
 import UIContext, { UI_MODE } from '../contexts/UIContext';
+import GameContext from '../contexts/GameContext';
 
 export default function EquipmentDialog() {
     const { setMode } = useContext(UIContext);
+    const { gameEquipment, characters } = useContext(GameContext);
+    const [tempAssign, setTempAssign] = useState({});
 
     return <DialogBase onClose={() => {
         setMode(UI_MODE.GAME);
@@ -12,6 +15,42 @@ export default function EquipmentDialog() {
         <div>
             <div>
                 Unassigned equipment
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Assign</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {gameEquipment.map((equipment, index) => {
+                            return <tr>
+                                <td>{equipment.type}</td>
+                                <td>
+                                    <select onChange={(e) => {
+                                        setTempAssign({
+                                            ...tempAssign,
+                                            [index]: e.target.value,
+                                        });
+                                    }}>
+                                        {characters.map((character) => {
+                                            return <option value={character.type}>{character.type}</option>
+                                        })}
+                                    </select>
+                                    <button onClick={() => {
+                                        const character = tempAssign[index];
+                                        if (!character) {
+                                            return;
+                                        }
+                                    }}>
+                                        Assign
+                                    </button>
+                                </td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
             </div>
             <div>
                 Assigned equipment
